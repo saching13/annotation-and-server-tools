30dbdc20f238f9b0376aaeb4525321612b37351b
Sachin Guruswamy <saching12@Sachins-MBP.hsd1.ca.comcast.net>
2018-10-19 00:55:52 -0700

    Reading video file

--- frame_setter.py
+++ frame_setter.py
@@ -1,2 +1,22 @@
 import cv2
 
+
+#def fps_modifier():
+
+
+
+
+
+
+vid_cap = cv2.VideoCapture('big_buck_bunny_720p_5mb.mp4')
+success,image = vid_cap.read()
+count = 0
+while success:
+  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+  success,image = vid_cap.read()
+  print('Read a new frame: ', success)
+  count += 1
+
+
+
+
