ddebbdbb2b52fa4d4cbe750d434c3f7881e0ca78
Sachin Guruswamy <saching12@Sachins-MBP.hsd1.ca.comcast.net>
2018-10-19 00:19:08 -0700

    bug fixes

--- raspberry_socket.py
+++ raspberry_socket.py
@@ -1,10 +1,11 @@
+#works only when run over raspberry
 import socket
 import thread
 import os
 import sys
 
 
-def Sendimages( threadName, c):
+def Sendimages(threadName, c):
     dir_path = os.path.dirname(os.path.realpath(__file__))
     while True:
        c.send("Next image")
@@ -14,7 +15,7 @@ def Sendimages( threadName, c):
        c.send(l)
        f.close()
        data = c.recv(4064)
-       print data
+       print(data)
        #if(c.recv(4064))
 
 
@@ -23,15 +24,15 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     # Create a socket obje
 host = socket.gethostname()# Get local machine name
 hostname = host + ".local"
 ip = socket.gethostbyname_ex(hostname)[1] # Get local machine name
-print socket.gethostbyname_ex(hostname)[2]
+print(socket.gethostbyname_ex(hostname)[2])
 port = 1500                # Reserve a port for your service.
 s.bind((host, port))
-print 'Got connection from', ip
+print('Got connection from', ip)
 s.listen(1)  # Now wait for client connection.
 
 while True:
    c, addr = s.accept()     # Establish connection with client.
-   print 'Got connection from', addr
+   print ('Got connection from', addr)
    thread.start_new_thread(Sendimages, ("Thread-1", c))
    c.send('Thank you for connecting')
    c.close()
