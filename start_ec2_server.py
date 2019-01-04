a1089d8dd5bd5c1f920c101cd17a45f8e81681fd
Sachin Guruswamy <43363595+saching13@users.noreply.github.com>
2019-01-03 23:26:16 -0700

    Update start_ec2_server.py

--- start_ec2_server.py
+++ start_ec2_server.py
@@ -1,11 +1,11 @@
 import boto.ec2
 import datetime
-conn = boto.ec2.connect_to_region("us-west-2",aws_access_key_id='AKIAJPXNQTF2TT5HKAAA',aws_secret_access_key='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')
+conn = boto.ec2.connect_to_region("us-west-2",aws_access_key_id='enter the key',aws_secret_access_key='enter key')
 present_time =datetime.datetime.now().replace(second=0, microsecond=0)
 next_time = datetime.datetime.now().replace(second=0, microsecond=0) + datetime.timedelta(minutes = 10)
 print(present_time)
 print (next_time)
-conn.start_instances(instance_ids=['i-09bf0786ee02b7ade'])
+conn.start_instances(instance_ids=['id'])
 serveron = True
 print ("server started")
 print (present_time.time())
@@ -13,17 +13,17 @@ while True:
     present_time = datetime.datetime.now().replace(second=0, microsecond=0)
     if next_time.time() <= present_time.time():
         if serveron:
-            conn.stop_instances(instance_ids=['i-09bf0786ee02b7ade'])
+            conn.stop_instances(instance_ids=[''])
             serveron=False
             print("server stopped")
             print(present_time)
         else:
-            conn.start_instances(instance_ids=['i-09bf0786ee02b7ade'])
+            conn.start_instances(instance_ids=['id'])
             serveron = True
             print("server started")
             print(present_time)
         next_time = datetime.datetime.now().replace(second=0, microsecond=0) + datetime.timedelta(minutes=10)
 
-#conn.start_instances(instance_ids=['i-09bf0786ee02b7ade'])
-#conn.stop_instances(instance_ids=['i-09bf0786ee02b7ade'])
+#conn.start_instances(instance_ids=['id'])
+#conn.stop_instances(instance_ids=['id'])
 
